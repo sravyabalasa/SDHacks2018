5a7220769fbaf4f33f86ead1cbd295406714ebe6
Titan-Ngo <42155527+Titan-Ngo@users.noreply.github.com>
2018-10-13 12:00:07 -0700

    wtf is happening

--- Main.py
+++ Main.py
@@ -41,7 +41,6 @@ cherryBlossom = picPredict('https://www.telegraph.co.uk/content/dam/Travel/Desti
 print("Your image mostly has: ")
 for i in cherryBlossom:
 	print("Concept: " + i[0])
-	print("Percentage Similarity: " + float(i[1])*100)
-	print()
+	print("Percentage Similarity: " + str(float(i[1])*100))
 
 
