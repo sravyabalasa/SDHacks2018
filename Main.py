17b4bd6f0401ea9d77a4ef6e612f015666c43eec
Stanley Lee <stanleylee91776@gmail.com>
2018-10-12 23:27:51 -0700

    shiba inu training

--- Main.py
+++ Main.py
@@ -2,4 +2,7 @@ from clarifai.rest import ClarifaiApp
 app = ClarifaiApp(api_key='9f4c8fdd79b24c4aa14ed9f3fec060e2')
 
 model = app.public_models.general_model
-response = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+response = model.predict_by_url(url='https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12224412/Shiba-Inu-On-White-01.jpg')
+concepts = response['outputs'][0]['data']['concepts']
+for concept in concepts:
+    print(concept['name'], concept['value'])
