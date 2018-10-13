155f4930807fcea7e34ac0b8090989a000ffb73d
sravyabalasa <sravyabalasa@users.noreply.github.com>
2018-10-13 02:45:54 -0700

    Comments
    More comments

--- Main.py
+++ Main.py
@@ -13,11 +13,18 @@ WebApp that displays Clarifai's normal results in classifying images of vacation
 
 #Import Clarifai
 from clarifai.rest import ClarifaiApp
-app = ClarifaiApp(api_key='9f4c8fdd79b24c4aa14ed9f3fec060e2')
+
+#Creates an "app" that runs Clarifai
+CLARIFAI_API_KEY = '9f4c8fdd79b24c4aa14ed9f3fec060e2'
+app = ClarifaiApp(api_key = CLARIFAI_API_KEY)
+
+#Training model for images, can be specified
 model = app.public_models.general_model
 
-
-def picPredict( imageLink ): #url should be a string
+#PREDICT API
+#Returns a list of concepts in an image based on model
+#Used for access in localHost to imageLink
+def picPredict( imageLink ): #URL should be a string for process
     response = model.predict_by_url(url= imageLink)
     concepts = response['outputs'][0]['data']['concepts']
 
@@ -26,6 +33,7 @@ def picPredict( imageLink ): #url should be a string
 
     return results
 
+#Passing cherry blossom through model
 response = model.predict_by_url(url='https://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/Japan/cherry-blossom-hirosaki-park-japan.jpg?imwidth=450')
 concepts = response['outputs'][0]['data']['concepts']
 for concept in concepts:
@@ -33,6 +41,7 @@ for concept in concepts:
 
 print('--------')
 
+#Passing through model
 response2 = model.predict_by_url(url='https://static.tripzilla.com/thumb/7/e/102014_800x.jpg')
 concepts = response2['outputs'][0]['data']['concepts']
 for concept in concepts:
--- TestSearch.py
+++ TestSearch.py
@@ -24,7 +24,12 @@ EXTENSION #4
 """
 
 from clarifai.rest import ClarifaiApp
-app = ClarifaiApp(api_key='9f4c8fdd79b24c4aa14ed9f3fec060e2')
+from clarifai.rest import Image as C1Image
+
+#Creates an "app" that runs Clarifai
+CLARIFAI_API_KEY = '9f4c8fdd79b24c4aa14ed9f3fec060e2'
+app = ClarifaiApp(api_key = CLARIFAI_API_KEY)
+
 
 search = app.inputs.search_by_image(url='https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12224412/Shiba-Inu-On-White-01.jpg')
 
