3758a417768c465904817e58433341195a857a54
sravyabalasa <sravyabalasa@users.noreply.github.com>
2018-10-13 10:17:59 -0700

    Update TestSearch.py

--- TestSearch.py
+++ TestSearch.py
@@ -33,18 +33,13 @@ from clarifai.rest import Image as ClImage
 CLARIFAI_API_KEY = '9f4c8fdd79b24c4aa14ed9f3fec060e2'
 app = ClarifaiApp(api_key = CLARIFAI_API_KEY)
 
+#Text file of image URLS
+FILE_NAME = 'vacations_txt'
+FILE_PATH = os.path.join(os.path.curdir, FILE_NAME)
+
 
 #Training model for images, can be specified
 model = app.models.get('travel-v1.0')
-
-#Adding image to search index
-img1 = ClImage(url="https://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/Japan/cherry-blossom-hirosaki-park-japan.jpg?imwidth=450")
-img2 = ClImage(url="https://samples.clarifai.com/puppy.jpeg")
-img3 = ClImage(url="https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/50/2017/04/J1GDDB-920x614.jpg")
-img4 = ClImage(url="https://www.tahiti.com/images1/thumbs/Borabora-LeMeridien_600x360.jpg")
-
-app.inputs.bulk_create_images([img1, img2])
-
 
 #PREDICT API
 #Returns a list of concepts in an image based on model
@@ -61,27 +56,25 @@ def picPredict( imageLink ): #URL should be a string for process
 
     return results
 
-'''
 #Searching by image
-search = app.inputs.search_by_image(url='https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12224412/Shiba-Inu-On-White-01.jpg')
+search = app.inputs.search_by_image(url='https://japan-magazine.jnto.go.jp/jnto2wm/wp-content/uploads/1608_special_TOTO_main.jpg')
 
 for search_result in search:
     print("Similarity: ", search_result.score, "| URL: ", search_result.url)
-'''
 
-#Search by concepts
-image = picPredict('https://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/Japan/cherry-blossom-hirosaki-park-japan.jpg?imwidth=450')
+image = picPredict('https://japan-magazine.jnto.go.jp/jnto2wm/wp-content/uploads/1608_special_TOTO_main.jpg')
 
 #Concepts in given image
 print("Your image mostly has: ")
-for i in cherryBlossom:
+for i in image:
 	print("Concept: " + i[0])
-	print("Percentage Similarity: " + float(i[1])*100)
+	print("Probability: " + i[1])
 	print()
 
+'''
 #Search by multiple concepts with name
-searchOne = app.inputs.search_by_predicted_concepts(concepts=[image[0][0], image[1][0], image[2][0], image[3][0], image[4][0]])
 
+searchOne = app.inputs.search_by_predicted_concepts(concepts=[image[0][0], image[1][0], image[2][0], image[3][0], image[4][0]])
 for search_result in searchOne:
 	print("Similarity: ", search_result.score, "| URL: ", search_result.url)
-
+'''
