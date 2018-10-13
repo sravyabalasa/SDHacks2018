c956a720a7bcc53d682fd39340cf3d042c8ac958
Titan-Ngo <42155527+Titan-Ngo@users.noreply.github.com>
2018-10-13 01:42:15 -0700

    Deletes useless files
    Also moves location of website/css to "templates" folder

--- Searchrecognition.py
+++
@@ -1,8 +0,0 @@
-from clarifai.rest import ClarifaiApp
-from clarifai.rest import Image as ClImage
-app = ClarifaiApp(api_key='9f4c8fdd79b24c4aa14ed9f3fec060e2')
-
-model = app.models.get('general-v1.3')
-
-model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg', min_value=0.97)
-print
