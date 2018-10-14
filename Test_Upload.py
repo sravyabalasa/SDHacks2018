fbe721b59b9520b76d70ff7d9db686777be9f029
sravyabalasa <sravyabalasa@users.noreply.github.com>
2018-10-14 02:38:49 -0700

    Temp_Final Commit
    Finished functionality of Upload and Search in back end functions to pass onto front end dev

--- Test_Upload.py
+++ Test_Upload.py
@@ -1,46 +1,48 @@
-# UPLOAD.PY
-import os
-import time
-from clarifai.rest import ClarifaiApp
-from clarifai.rest import Image as ClImage
-
-#Creates Clarifai App
-app = ClarifaiApp(api_key='549be605f54443cd8bf67b33e6061169')
-app.inputs.delete_all()
-
-#URL
-URL_FILE_NAME = 'vacations_url.txt'
-URL_FILE_PATH = os.path.join(os.path.curdir, URL_FILE_NAME)
-# Counter variables
-current_batch = 0
-counter = 0
-with open(URL_FILE_PATH) as data_file:
-    images = [url.strip() for url in data_file]
-    row_count = len(images)
-    print("Total number of images:", row_count)
-
-#Making images into a list --> SERVER
-imageList = []
-for i in range(row_count):
-    print("Processing image: #", (counter+1))
-    imageList.append(ClImage(url=images[i], allow_dup_url=False))
-    counter=counter+1
-app.inputs.bulk_create_images(imageList)
+'''
+Filename: Test_Upload.py
+Author:   Main: Sravya Balasa
+          Partners: Jill Su, Titan Ngo, Stanley Lee
+Date:     10/14/18
+'''
 
 '''
-#NAMES
-NAMES_FILE_NAME = 'vacations_names.txt'
-NAMES_FILE_PATH = os.path.join(os.path.curdir, NAMES_FILE_NAME)
-# Counter variables
-current_batch = 0
-counter = 0
-with open(NAMES_FILE_PATH) as data_file:
-    names = [url.strip() for url in data_file]
-    row_count = len(names)
-    print("Total number of names:", row_count)
+run() 
 
-#Making names into a list
-nameList = []
-for i in range(row_count):
-    nameList.append(names[i])
+Uploads all vacation images to the server
+No output
+Loaded every time website is reloaded
 '''
+def run():
+
+    #Import statements + constructors
+    import os
+    from clarifai.rest import ClarifaiApp
+    from clarifai.rest import Image as ClImage
+
+    #Creates Clarifai App
+    app = ClarifaiApp(api_key='a589bc8304c042419bcb4077b269e897')
+    #Resets the server to 0 images, avoids duplicates
+    app.inputs.delete_all()
+
+    #URL
+    URL_FILE_NAME = 'vacations_url.txt'
+    URL_FILE_PATH = os.path.join(os.path.curdir, URL_FILE_NAME)
+
+    #Reads the urls of images from txt file
+    with open(URL_FILE_PATH) as data_file:
+        images = [url.strip() for url in data_file]
+        row_count = len(images)
+
+    #Making images into a list that convers into being uploadable to the server
+    imageList = []
+    for i in range(row_count):
+        imageList.append(ClImage(url=images[i], allow_dup_url=False))
+    app.inputs.bulk_create_images(imageList)
+
+    #NAMES
+    NAMES_FILE_NAME = 'vacations_names.txt'
+    NAMES_FILE_PATH = os.path.join(os.path.curdir, NAMES_FILE_NAME)
+    
+    return
+
+run()
