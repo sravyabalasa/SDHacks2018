433fca69d5459cebcf6845651b96ea14de885085
Titan-Ngo <42155527+Titan-Ngo@users.noreply.github.com>
2018-10-13 18:04:40 -0700

    templates stuff

--- LocalHost.py
+++ LocalHost.py
@@ -36,7 +36,8 @@ def results():
     place2 = placeList[1]
     place3 = placeList[2]'''
 
-    return render_template('test.html', concept=concept)''' percentage=percentage, name1=place1[0], name2=place2[1], name3=place3[2], image1=place1[1], image2=place2[1], image3=place3[1])'''
+    return render_template('test.html', concept=concept, percentage=percentage)
+''' name1=place1[0], name2=place2[1], name3=place3[2], image1=place1[1], image2=place2[1], image3=place3[1])'''
 
 # run local host
 if __name__ == "__main__":
